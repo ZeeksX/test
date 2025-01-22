cbd4634011d73c20dd2261a463ca108887a9959e
ZeeksX <ikinwotezekiel@gmail.com>
2025-01-22 17:44:51 +0100

    test links

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
-
+import { useNavigate } from 'react-router'
 const Landing = () => {
+  const navigate = useNavigate();
+
   return (
-    <div>Landing</div>
+    <>
+      <div>Landing</div>
+      <h3 className='cursor-pointer hover:underline' onClick={()=>{
+        navigate("/login")
+      }}>Login Page</h3>
+      <h3 className='cursor-pointer hover:underline' onClick={()=>{
+        navigate("/signup")
+      }}>Sign up page</h3>
+      <h3 className='cursor-pointer hover:underline' onClick={()=>{
+        navigate("/onboarding")
+      }}>Onboarding</h3>
+    </>
+
   )
 }
 
